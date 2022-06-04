75196363ce435a80c5e69df6f10e63f331a834ed
Domenic <domenic.digiovanni@protonmail.com>
2022-06-04 18:43:06 -0400

    Create skeleton AutoBrake, listing 10-4

--- chapter-10-testing/examples/AutoBrake.h
+++ chapter-10-testing/examples/AutoBrake.h
@@ -2,9 +2,20 @@
 
 template <typename T>
 struct AutoBrake {
-    AutoBrake(const T& Publish);
-    void observe(const SpeedUpdate&);
-    void observe(const CarDetected&);
+    AutoBrake(const T& Publish) : publish{ publish } { };
+    void observe(const SpeedUpdate& su) { };
+    void observe(const CarDetected& cd) { };
+    void set_collision_threshold_s(double x) {
+        collision_threshold_s = x;
+    }
+    double get_collision_threshold_s() const {
+        return collision_threshold_s;
+    }
+    double get_speed_mps() const {
+        return speed_mps;
+    }
 private:
+    double collision_threshold_s;
+    double speed_mps;
     const T& publish;
 };
