24d4c941bca1a37ab4dc1fa8b131f700ef521052
Domenic <domenic.digiovanni@protonmail.com>
2022-06-06 22:34:45 -0400

    Refactor AutoBrake to use IServiceBus

--- chapter-10-testing/examples/AutoBrake.h
+++ chapter-10-testing/examples/AutoBrake.h
@@ -5,25 +5,13 @@
 #include <stdexcept>
 
 #include "AutoBrakePods.h"
+#include "IServiceBus.h"
 
 
-template <typename T>
 struct AutoBrake {
-    AutoBrake(const T& publish)
+    AutoBrake(IServiceBus& bus)
         : collision_threshold_s{ 5 },
-          speed_mps{},
-          publish{ publish } { };
-    void observe(const SpeedUpdate& su) {
-        speed_mps = su.velocity_mps;
-    }
-    void observe(const CarDetected& cd) {
-        const auto relative_velocity_mps = speed_mps - cd.velocity_mps;
-        const auto time_to_collision_s = cd.distance_m / relative_velocity_mps;
-        if (time_to_collision_s > 0 &&
-            time_to_collision_s <= collision_threshold_s) {
-                publish(BrakeCommand{ time_to_collision_s });
-        }
-    }
+          speed_mps{} { };
     void set_collision_threshold_s(double x) {
         if (x < 1) throw std::runtime_error{ "Collision less than 1." };
         collision_threshold_s = x;
@@ -37,5 +25,4 @@ struct AutoBrake {
 private:
     double collision_threshold_s;
     double speed_mps;
-    const T& publish;
 };
